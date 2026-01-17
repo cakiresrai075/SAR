39c0c25a156912b4372dc69632013b671582c228
cakiresrai075 <cakiresrai075@gmail.com>
2026-01-17 17:11:37 +0300

    Update scorecam.py

--- SSSD/CAM/scorecam.py
+++ SSSD/CAM/scorecam.py
@@ -31,7 +31,7 @@ ALPHA = 0.55
 N_IMAGES = 100 #deneme hepsini görmek için None
 
 # Target layers (YOLOv5 backbone + neck layers)
-TARGET_LAYERS = [8, 9, 17, 20, 22, 23]  # deneme sonucu en iyi 22
+TARGET_LAYERS = [8, 9, 17, 20, 22, 23]  # deneme sonucu en iyi 8
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Score-CAM specific settings
