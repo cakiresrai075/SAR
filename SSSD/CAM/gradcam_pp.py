d1e8c15b494a36b473991d440b267d94a0f700f9
cakiresrai075 <cakiresrai075@gmail.com>
2026-01-17 15:42:25 +0300

    Add paths for weights, images, and output directory

--- SSSD/CAM/gradcam_pp.py
+++ SSSD/CAM/gradcam_pp.py
@@ -1,1 +1,4 @@
-
+WEIGHTS = "/content/drive/MyDrive/SAR/SSDD_yolov5/train/weights/best.pt"
+IMG_DIR = "/content/drive/MyDrive/SAR/SSDD/images/test"
+OUT_DIR = "/content/drive/MyDrive/CAM_OUT/SSDD/GRADCAM"
+os.makedirs(OUT_DIR, exist_ok=True)
